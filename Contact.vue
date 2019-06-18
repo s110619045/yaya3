48615f46e0d4e74fda79a60851461c2acc9fa712
s110619045 <49371664+s110619045@users.noreply.github.com>
2019-06-18 17:51:00 +0800

    Add files via upload

---
+++ Contact.vue
@@ -0,0 +1,25 @@
+<template>
+    <div  class="contact">
+        <img class="contact_img" src="../assets/contact.jpg">
+        <div class="contact_text">
+            <p class="pt">Name</p>
+            <p class="pi">黃翔莉(左) 李欣霏(右）</p>
+            <p class="pt">EMAIL</p>
+            <p class="pi">s110619045@stu.ntue.edu.tw</p>
+            <p class="pi">s110619046@stu.ntue.edu.tw</p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+   name: 'Contact'
+}
+</script>
+
+<style lang="postcss" scoped>
+@import "../styles/base/_variables.css";
+@import "../styles/base/_global.css";
+@import "../styles/modules/_contact.css";
+</style>
---
+++ ContactPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container_1">
+    <Nav/>
+    <Contact/>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import { mapGetters } from 'vuex';
+
+import Nav from "../components/Nav";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+
+export default {
+  name: "ContactPage",
+  components: {
+    Nav,
+    Contact,
+    Footer
+  },
+  computed: mapGetters(['totalNews', 'selectedNews']),
+  methods: mapActions(['init']),
+  created() {
+    this.init();
+  }
+};
+</script>
+
+<style lang="postcss">
+@import "../styles/base/_variables.css";
+@import "../styles/base/_global.css";
+</style>
+
