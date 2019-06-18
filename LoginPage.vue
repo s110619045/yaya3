7cba28a7147b078a60ff77956d2f7136d9b5bdc3
s110619045 <49371664+s110619045@users.noreply.github.com>
2019-06-18 17:45:01 +0800

    Add files via upload

---
+++ LoginPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container_1">
+    <!-- <Nav/> -->
+    <Login/>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import { mapGetters } from 'vuex';
+
+import Nav from "../components/Nav";
+import Login from "../components/Login";
+import Footer from "../components/Footer";
+
+export default {
+  name: "LoginPage",
+  components: {
+    // Nav,
+    Login,
+    Footer
+  },
+  computed: mapGetters(['totalNews', 'selectedNews']),
+  methods: mapActions(['init']),
+  created() {
+    this.init();
+  }
+};
+</script>
+
+<style lang="postcss">
+@import "../styles/base/_variables.css";
+@import "../styles/base/_global.css";
+</style>
+
